d5ccdf7c002c2845e967d1fd9a9d9fdfd103fb27
YashodhaSamarawickrama <yasho.samarawickrama@gmail.com>
2018-04-10 11:52:42 +0530

    Mongoose dependancy added

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
-const app = express();
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
+
+
 
 app.listen(port,()=>{
     console.log("Server is up and running on http://www.localhost: "+port);

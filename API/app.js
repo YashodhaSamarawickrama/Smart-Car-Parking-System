777539eae9b09f796d61f03f0e2f86cf0324d2b5
YashodhaSamarawickrama <yasho.samarawickrama@gmail.com>
2018-04-11 15:46:05 +0530

    Route register added

--- API/app.js
+++ API/app.js
@@ -2,16 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const slotRoutes = require('./routes/slots/Slots');
+
 //var db = require('./db');
 
 //var UserController = require('./user/UserController');
 
 //.use is used to make the middleware for the app variable. All the incoming requests will go through it
 
-app.use((req,res,next)=>{
-    res.status(200).json({
-        message:"The code works"
-    })
-});
+app.use('/Slots',slotRoutes);
 
 module.exports = app;
--- API/routes/slots/Slots.js
+++ API/routes/slots/Slots.js
@@ -1,12 +1,41 @@
-var mongoose = require('mongoose');
+const express = require('express');
+
+//To register different routes
+
+const router = express.Router();
+
+//when a GET request comes for a slot
+
+router.get('/',(req,res,next)=>{
+    res.status(200).json({
+        message:'Handling GET requests to /Slots'
+    });
+});
+
+//when a POST request comes for a slot
+
+router.post('/',(req,res,next)=>{
+    res.status(200).json({
+        message:'Handling POST requests to /Slots'
+    });
+});
+
+//when a GET request comes to a specific slot
+
+router.get('/:slotId',(req,res,next) => {
+    const id = req.params.slotId ;
+
+
+})
+
+//var mongoose = require('mongoose');
 //mongoose is used to interact with the Mongo Database
 //table to store the status of parking slots
-var SlotSchema = new mongoose.Schema({
+/*var SlotSchema = new mongoose.Schema({
      SlotID: Integer,
     Availability : String,
 
 
 });
-mongoose.model('Slot', UserSchema);
-
-module.exports = mongoose.model('Slot');
+mongoose.model('Slot', UserSchema);*/
+module.exports = router;

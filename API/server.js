7e3f01f0c9891411521686f4a79c10b60043be61
YashodhaSamarawickrama <yasho.samarawickrama@gmail.com>
2018-04-11 15:07:38 +0530

    JWT Dependancy

--- API/server.js
+++ API/server.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-
+const http = require('http');
+//const express = require('express');
+//const bodyParser = require('body-parser');
+//const path = require('path');
+//const cookieParser = require('cookie-parser');
+/*const exphbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;*/
+//const mongo = require('mongodb');
+//const mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/parkSMART');
+//const db = mongoose.connection;
 const app = require('./app');
-
 const port = process.env.PORT || 3000;
 
-
-
+//setting the socket to listen
 app.listen(port,()=>{
     console.log("Server is up and running on http://www.localhost: "+port);
 
